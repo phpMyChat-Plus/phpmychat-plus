90e05c0db27946c885528ee55489f02d95033239
ciprianmp <ciprianmp@ac0dd1e6-98dc-4d9d-8a75-0c608842e7c7>
2007-12-16 13:47:34 +0000

    release update - 1.92-f6

--- trunk/lib/release.lib.php
+++ trunk/lib/release.lib.php
@@ -1,8 +1,8 @@
 <?php
 define("APP_NAME", 'phpMyChat - Plus'); 	  // Application name
 define("APP_VERSION", '1.92');    				// Never modify/remove this line - it controls the update check up.
-define("APP_MINOR", '-f5');    				// Never modify/remove this line - it controls the update check up.
-define("RELEASE_DATE", 'Saturday, 24 November 2007');    		// Release date of this version
+define("APP_MINOR", '-f6');    				// Never modify/remove this line - it controls the update check up.
+define("RELEASE_DATE", 'Sunday, 16 December 2007');    		// Release date of this version
 define("BASED_ON", 'phpMyChat 0.14.5 by Nick Hozey');    // Original standard version
 define("PLUS_DEVELOPER", 'Ciprian Murariu');    // Plus versions
 ?>
