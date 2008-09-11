445e98f7c74d16cdf0a9a72836783c960ace6b47
Ciprian Murariu <ciprianmp@users.noreply.github.com>
2008-09-11 09:36:19 +0000

    1.93-RC4 - sessions warning fix

--- logout.php
+++ logout.php
@@ -1,7 +1,11 @@
 <?php
+	ini_set('session.bug_compat_42',0);
+	ini_set('session.bug_compat_warn',0);
+	ini_set('session.use_trans_sid',1);
 	$_SESSION["adminlogged"] = NULL;
 	unset($_SESSION["adminlogged"]);
 	$_SESSION = array();
-	session_unset();
+	session_start();
 	session_destroy();
+	session_unset();
 ?>
