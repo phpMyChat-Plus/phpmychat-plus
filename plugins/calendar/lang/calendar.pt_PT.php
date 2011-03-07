8aba763262b7839779a69d30a2ff79abc5218868
Ciprian Murariu <ciprianmp@users.noreply.github.com>
2011-03-07 21:38:57 +0000

    1.94-Beta6 - Portuguese (Portugal) calendar addition/fix.

--- plugins/calendar/lang/calendar.pt_PT.php
+++ plugins/calendar/lang/calendar.pt_PT.php
@@ -1,30 +1,30 @@
-<?php
-# pt_BR translation for
+﻿<?php
+# pt_PT translation for
 # PHP-Calendar, DatePicker Calendar class: http://www.triconsole.com/php/calendar_datepicker.php
-# Version: 2.30
-# Language: Português (Brasil)
-# Translator: Jaime Dias ( jaime@jotadias.com.br)
-# Last file update: 29.07.2010
+# Version: 3.3
+# Language: Portuguese (Portugal) / Português (Portugal)
+# Translator: Developer Tuga <developer.tuga@gmail.com>
+# Last file update: 07.03.2011
 
 // Class strings localization
 define("L_DAY", "Dia");
 define("L_MONTH", "Mês");
 define("L_YEAR", "Ano");
 define("L_PREV", "Anterior");
-define("L_NEXT", "Próximo");
-define("L_REF_CAL", "Atualizando...");
-define("L_CHK_VAL", "Verifique data");
-define("L_SEL_LANG", "Selecione Idioma");
-define("L_SEL_ICON", "Selecione Icon");
-define("L_SEL_DATE", "Selecione Data");
-define("L_ERR_SEL", "Sua Seleção não é válida");
-define("L_NOT_ALLOWED", "Esta data não é permitida ser selecionada");
+define("L_NEXT", "Seguinte");
+define("L_REF_CAL", "A actualizar o calendário...");
+define("L_CHK_VAL", "Verifica o valor");
+define("L_SEL_LANG", "Seleccione Língua");
+define("L_SEL_ICON", "Seleccione Icon");
+define("L_SEL_DATE", "Seleccione Data");
+define("L_ERR_SEL", "A sua escolha não é válida");
+define("L_NOT_ALLOWED", "Esta data não pode ser seleccionada");
 
 // Set the first day of the week in your language (0 for Sunday, 1 for Monday)
-define("FIRST_DAY", "0");
+define("FIRST_DAY", "1");
 
 // Months Long Names
-define("L_JAN", "janneiro");
+define("L_JAN", "janeiro");
 define("L_FEB", "fevereiro");
 define("L_MAR", "março");
 define("L_APR", "abril");
@@ -55,7 +55,7 @@ define("L_TUE", "terça");
 define("L_WED", "quarta");
 define("L_THU", "quinta");
 define("L_FRI", "sexta");
-define("L_SAT", "sabado");
+define("L_SAT", "sábado");
 define("L_SUN", "domingo");
 // Week days Short Names
 define("L_S_MON", "seg");
@@ -63,18 +63,18 @@ define("L_S_TUE", "ter");
 define("L_S_WED", "qua");
 define("L_S_THU", "qui");
 define("L_S_FRI", "sex");
-define("L_S_SAT", "sab");
+define("L_S_SAT", "sáb");
 define("L_S_SUN", "dom");
 
 // Windows encoding
 define("WIN_DEFAULT", "windows-1252");
 define("L_CAL_FORMAT", "%d %B %Y");
-if(!defined("L_LANG") || L_LANG == "L_LANG") define("L_LANG", "pt_BR"); // en_US format of your language
+if(!defined("L_LANG") || L_LANG == "L_LANG") define("L_LANG", "pt_PT"); // en_US format of your language
 
-// Set the BR specific date/time format
+// Set the PT specific date/time format
 if (stristr(PHP_OS,"win")) {
-setlocale(LC_ALL, "pt-BR.UTF-8", "pt-br", "Portuguese");
+setlocale(LC_ALL, "pt-PT.UTF-8", "pt-pt", "Portuguese");
 } else {
-setlocale(LC_ALL, "pt_BR.UTF-8", "por.UTF-8", "por_bra.UTF-8", "Portuguese.UTF-8");
+setlocale(LC_ALL, "pt_PT.UTF-8", "por.UTF-8", "por_por.UTF-8", "Portuguese.UTF-8");
 }
 ?>
